848a9b56d14864a98b52ab38f93bed2b76c263b4
alexandregjr <alexgalocha.jr@gmail.com>
2021-12-08 07:34:00 -0300

    adding todos

--- src/main/java/org/edpirro/alexandregjr/main/Converter.java
+++ src/main/java/org/edpirro/alexandregjr/main/Converter.java
@@ -6,6 +6,7 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.OWL2;
 import org.apache.jena.vocabulary.RDFS;
 
 import java.io.*;
@@ -17,40 +18,46 @@ import java.util.function.UnaryOperator;
 
 public class Converter {
     public static void main(String[] args) {
+        boolean DEBUG = true;
+
         Charset ch = StandardCharsets.UTF_8;
         String separator = "\t";
-        String filepath = "/home/alexandregjr/courses/scc0283/work/data.csv";
         String ns = "http://alexandregjr.edpirro.org/gbif#";
         UnaryOperator<String> nameToURI = s -> ns + s.toLowerCase().replace(" ", "_");
 
+        // TODO: import model, not create default
         Model m = ModelFactory.createDefaultModel()
                 .setNsPrefixes(PrefixMapping.Standard)
                 .setNsPrefix("gbif", ns);
-        Resource clazz = m.createResource(ns + "Animal", OWL.Class);
+        Resource clazz = m.createResource(ns + "Occurrence", OWL2.Class);
 
-        try (InputStream is = new FileInputStream(filepath);
+        try (InputStream is = new FileInputStream("data.csv");
              Reader r = new InputStreamReader(is, ch);
              BufferedReader br = new BufferedReader(r)) {
             String first = br.lines().findFirst().orElseThrow(IllegalArgumentException::new);
             List<Property> props = Arrays.stream(first.split(separator))
-                    .map(s -> m.createResource(nameToURI.apply(s), OWL.DatatypeProperty)
+                    .map(s -> m.createResource(nameToURI.apply(s), OWL2.DatatypeProperty)
                             .addProperty(RDFS.label, s).as(Property.class)).toList();
-//            br.lines().forEach(line -> {
-//                String[] data = line.split(separator);
-//                if (data.length != props.size()) throw new IllegalArgumentException();
-//                Resource individual = m.createResource(clazz);
-//                for (int i = 0; i < data.length; i++) {
-//                    individual.addProperty(props.get(i), data[i]);
-//                }
-//            });
-            String line = br.lines().findFirst().orElseThrow(NullPointerException::new);
-            String[] data = line.split(separator);
-            if (data.length != props.size()) throw new IllegalArgumentException();
-            Resource individual = m.createResource(clazz);
-            for (int i = 0; i < data.length; i++) {
-                individual.addProperty(props.get(i), data[i]);
+            if (!DEBUG) {
+                br.lines().forEach(line -> {
+                    String[] data = line.split(separator);
+                    if (data.length != props.size()) throw new IllegalArgumentException();
+                    Resource individual = m.createResource(clazz);
+                    // TODO: filter properties
+                    for (int i = 0; i < data.length; i++) {
+                        individual.addProperty(props.get(i), data[i]);
+                    }
+                });
+            } else {
+                String line = br.lines().findFirst().orElseThrow(NullPointerException::new);
+                String[] data = line.split(separator);
+                if (data.length != props.size()) throw new IllegalArgumentException();
+                Resource individual = m.createResource(clazz);
+                // TODO: filter properties
+                for (int i = 0; i < data.length; i++) {
+                    individual.addProperty(props.get(i), data[i]);
+                }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
